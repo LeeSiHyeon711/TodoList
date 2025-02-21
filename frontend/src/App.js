552e39efa6ce6ef44b0368d258278b7f42ab2ada
DESKTOP-D1SJMHC\1 <dltlgus1230@naver.com>
2025-02-21 15:12:58 +0900

    feat: 검색 기능 추가

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import Todo from './Todo';
 import React, { useState, useEffect } from "react";
-import { Container,List,Paper } from "@mui/material";
+import { Container,List,Paper,Grid, TextField } from "@mui/material";
 import AddTodo from './AddTodo';
 import { call } from './service/ApiService';
 
 function App() {
   const [items, setItems] = useState([]);
+  const [search, setSearch] = useState("");
 
   useEffect(() => {
     call("/api/todos", "GET", null)
@@ -15,19 +16,28 @@ function App() {
     });
   }, []);
 
+  const getSearchResult = () => {
+    if (search.trim() === "") return items;
+    return items.filter((item) => item.title.toLowerCase().includes(search.toLowerCase()));
+  };
+
+  const onChangeSearch = (e) => {
+    setSearch(e.target.value);
+  };
+
   const editItem = (item) => {
     call(`/api/todos/${item.id}`, "PUT", item)
     .then(() => {
       setItems((prevItems) => [...prevItems]);
     });
-  }
+  };
 
   const deleteItem = (item) => {
     call(`/api/todos/${item.id}`, "DELETE", item)
     .then(() => {
       setItems((prevItems) => prevItems.filter((e) => e.id !== item.id));
     });
-  }
+  };
 
   const addItem = (item) => {
     call("/api/todos", "POST", item)
@@ -36,10 +46,12 @@ function App() {
     });
   };
 
-  let todoItems = items.length > 0 && (
+  const filteredItems = getSearchResult();
+
+  let todoItems = filteredItems.length > 0 && (
     <Paper style={{ margin: 16}}>
       <List>
-        {items.map((item) => (
+        {filteredItems.map((item) => (
           <Todo item={item}
           key={item.id}
           editItem={editItem}
@@ -48,12 +60,25 @@ function App() {
       </List>
     </Paper>
   );
-  return (<div className="App">
+  return (
+        <div className="App">
           <Container maxWidth="md">
+          <TextField
+            value={search}
+            onChange={onChangeSearch}
+            className='searchbar'
+            placeholder='검색할 TODO를 입력하세요'
+          />
+            <div className='Header'>
+              <h3>오늘은 📝</h3>
+              <h1>{new Date().toDateString()}</h1>
+            </div>
             <AddTodo addItem={addItem}/>
-            <div className='TodoList'>{todoItems}</div>
+            <Grid className='TodoList'>
+              {todoItems}
+            </Grid>
           </Container>
-      </div>);
-}
+        </div>
+        )};
 
 export default App;
